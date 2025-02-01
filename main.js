2a2dbf7f991135036858cc6806a74e82c7f45851
mahdi_dev <hadjidjmahdi@gmail.com>
2025-02-01 22:01:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let passwordBox = document.getElementById('password');
+let length = 12;
+
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase ='abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+const allCharacters = uppercase+lowercase+numbers+symbols; 
+
+function generatePassword(){
+    let password = '';
+
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    
+    while(password.length < length){
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)]
+    }
+    passwordBox.value = password; 
+}
+
+function CopyPass(){
+    passwordBox.select();
+    navigator.clipboard.writeText(passwordBox.value);
+    console.log(`you copied this '${passwordBox.value}'`);
+    
+}
+
+window.addEventListener('load', ()=>{
+    passwordBox.value = '';
+})
+
